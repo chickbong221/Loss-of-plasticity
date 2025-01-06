427580fa73aa4c52de26fcd1e8f1b667a3966804
Le Tuan <letuanhf@gmail.com>
2025-01-06 23:02:37 +0700

    fix big

--- generated_cmds/input_permuted_mnist/fail_rerun.sh
+++ generated_cmds/input_permuted_mnist/fail_rerun.sh
@@ -1,3 +1,4 @@
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network small_fully_connected_tanh_gates --n_samples 1000000 
 python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network fully_connected_tanh --n_samples 1000000 
 python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network small_fully_connected_tanh --n_samples 1000000 
 python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network fully_connected_tanh --n_samples 1000000 
@@ -5,3 +6,17 @@ python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr
 python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network fully_connected_tanh_gates --n_samples 1000000 
 python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network small_fully_connected_tanh_gates --n_samples 1000000 
 python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network fully_connected_tanh_gates --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network fully_connected_crelu --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network fully_connected_crelu --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network fully_connected_crelu_gates --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network small_fully_connected_relu_gates --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network fully_connected_relu --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network small_fully_connected_relu --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network fully_connected_relu_gates --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --network fully_connected_relu_with_hooks --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network fully_connected_crelu_gates --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network small_fully_connected_relu_gates --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network fully_connected_relu --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network small_fully_connected_relu --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network fully_connected_relu_gates --n_samples 1000000 
+python3 core/run/run.py --task input_permuted_mnist --learner adam --seed 0 --lr 0.001 --weight_decay 0.01 --network fully_connected_relu_with_hooks --n_samples 1000000 
